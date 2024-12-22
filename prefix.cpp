cad89b5ed1bd20b668d8d69c44e10e8a8166c61b
Nupurbhoir <nupurbhoir21@gmail.com>
2024-12-22 12:47:01 +0530

    Add files via upload

---
+++ prefix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Count {
+public:
+    int value;
+    Count() : value(5) {}
+    void operator--() {
+        --value;
+    }
+    void display() {
+        cout << "Count: " << value << endl;
+    }
+};
+int main() {
+    Count count1;
+    --count1;
+    count1.display();
+    return 0;
+}
